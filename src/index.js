21c6b83d72bbf86f3a89df2544af8cf542321de9
shitanshu-277 <shitanshugoyal2002@gmail.com>
2023-08-25 13:35:50 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const {PORT}=require('./config/serverConfig');
 
 const apiRoutes=require('./routes/index');
 
-// const {User} = require('./models/index');
+const db = require('./models/index')
+
+//const {User,Role} = require('./models/index');
 // const bcrypt = require('bcrypt');
 //const UserRepository = require('./repository/user-repository');
 // const UserService=require('./services/user-service')
@@ -18,6 +20,14 @@ const prepareAndStartServer = ()=>{
     app.use('/api',apiRoutes);
     app.listen(PORT,async()=>{
         console.log(`Server Started on Port : ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+        // const u1 = await User.findByPk(4);
+        // const r1 = await Role.findByPk(1);
+        //u1.addRole(r1);
+        // const response = await u1.hasRole(r1);
+        // console.log(response);
         // const repo=new UserRepository();
         // const response = await repo.getById(1);
         // console.log(response);
